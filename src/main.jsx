02407b8c3bf3dcc23ee535d9700bda315abc35e9
MaviUK <141088900+MaviUK@users.noreply.github.com>
2026-06-11 18:40:54 +0100

    Load season label display fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './seasonFinishTimer.js'
 import './tableZones.js'
 import './seasonArchive.js'
 import './lastSeasonReview.js'
+import './seasonLabelFix.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
