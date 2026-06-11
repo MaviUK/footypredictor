3974d564e61b62231638da4384aaaf3ccd4abbab
MaviUK <141088900+MaviUK@users.noreply.github.com>
2026-06-11 20:02:44 +0100

    Load season history detail view

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './seasonArchive.js'
 import './lastSeasonReview.js'
 import './seasonLabelFix.js'
 import './profileSeasonLabels.js'
+import './profileSeasonOpen.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
