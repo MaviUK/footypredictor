b5aa621994755294788eb60d8eb7ff3b5f9bc251
MaviUK <141088900+MaviUK@users.noreply.github.com>
2026-06-11 17:55:46 +0100

    Load last season review screen

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import './seasonFinishTimer.js'
 import './tableZones.js'
 import './seasonArchive.js'
+import './lastSeasonReview.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
